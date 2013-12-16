a1e24e168fafc375ca4781445bc467840635417c
rleys <richard.leys@ziti.uni-heidelberg.de>
2013-12-16 13:46:39 +0100

    Updated convertFileToRFS to convert from file to file

--- examples/rfs_backport/convert.tcl
+++ examples/rfs_backport/convert.tcl
@@ -5,7 +5,10 @@ package require osys::rfg::generator::rfsbackport
 package require odfi::files
 
 
-
+## String convert
 set res [osys::rfg::generator::rfsbackport::convertFileToRFS ./rfs_backport.rf]
 
 odfi::files::writeToFile ./rfs_backport.xml $res
+
+## File to file 
+osys::rfg::generator::rfsbackport::convertFileToRFS ./rfs_backport.rf ./rfs_backport2.xml
