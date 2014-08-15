4d9555a50426900ac95605e148c38b32d58a9995
Tobias Markus <tobias_markus@gmx.net>
2014-08-15 14:12:50 +0200

    cleaning of some outdated unit-tests and files

--- examples/verilog/Example_RF.f
+++
@@ -1,2 +0,0 @@
-/home/tmarkus/modules-manager/install/rfg/examples/verilog/RF_Wrapper.v
-/home/tmarkus/modules-manager/install/rfg/examples/verilog/Example_RF.v
--- examples/verilog/test.f
+++
@@ -1,3 +0,0 @@
-/home/tmarkus/modules-manager/install/rfg/examples/verilog/RF_Wrapper.v
-/home/tmarkus/modules-manager/install/rfg/examples/verilog/test.v
-/home/tmarkus/modules-manager/install/rfg/examples/verilog/Example_RF.v
