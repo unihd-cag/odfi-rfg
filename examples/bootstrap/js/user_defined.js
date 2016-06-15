035ce1fe4f496dc24d648fa4c0d5e37a1df926d5
Sebastian Wittka <sebastian.wittka@gmail.com>
2016-06-15 13:01:53 +0200

    first layout for the new html documentation

---
+++ examples/bootstrap/js/user_defined.js
@@ -0,0 +1,23 @@
+$(function() {
+        
+  $('.list-group-item').on('click', function() {
+    $('.glyphicon', this)
+      .toggleClass('glyphicon-chevron-right')
+      .toggleClass('glyphicon-chevron-down');
+  });
+});
+
+$(function() {
+        
+  $('.clickable').on('click', function() {
+    $('.glyphicon', this)
+      .toggleClass('glyphicon-chevron-right')
+      .toggleClass('glyphicon-chevron-down');
+  });
+});
+
+jQuery(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.document.location = $(this).data("href");
+    });
+});
