53e0b5f29897ec3fb0fa807ba6a6299502578420
Sebastian Wittka <sebastian.wittka@gmail.com>
2016-06-24 13:09:21 +0200

    files for user defined css and js added

---
+++ tcl/generator-htmlbrowser/user_defined.js
@@ -0,0 +1,32 @@
+$(function() {
+        
+  $('.list-group-item .glyphicon').on('click', function() {
+    $(this)
+      .toggleClass('glyphicon-chevron-right')
+      .toggleClass('glyphicon-chevron-down');
+  });
+});
+
+$(function() {
+        
+  $('.clickable').on('click', function() {
+    $(this)
+      .toggleClass('glyphicon-chevron-right')
+      .toggleClass('glyphicon-chevron-down');
+  });
+});
+
+$(function() {
+        
+  $('.clickable-row').on('click', function() {
+    $('.glyphicon' ,this)
+      .toggleClass('glyphicon-chevron-right')
+      .toggleClass('glyphicon-chevron-down');
+  });
+});
+
+jQuery(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.document.location = $(this).data("href");
+    });
+});
