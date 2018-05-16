7d663f70ebde67afbdcf7cbe57f50df36a5f6827
Tobias Markus <tobias_markus@gmx.net>
2018-05-16 15:25:04 +0200

    Quick Fix for build script

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,10 @@ mkdir RFG.vfs
 mkdir RFG.vfs/lib
 mv odfi-rfg RFG.vfs/lib/odfi-rfg
 cd RFG.vfs/lib/
-git clone -b legacy https://github.com/unihd-cag/odfi-dev-tcl.git
+git clone https://github.com/unihd-cag/odfi-dev-tcl.git
+cd odfi-dev-tcl/
+git checkout 2a2c6c0a3b8cd339ac80e843ed56a7b72592e3de
+cd ../
 cd ../../
 mv RFG.vfs/lib/odfi-rfg/build/main.tcl RFG.vfs/main.tcl
 mv RFG.vfs/lib/odfi-rfg/build/basekit basekit
